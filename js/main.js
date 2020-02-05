ac88251ec9c504ffd09dcc1dc5128e0b455475cb
leer <ladesigns90@gmail.com>
2020-02-05 16:48:28 -0500

    Added for each statement in js file to attach event listener to all div elements with modal-btn class.

--- js/main.js
+++ js/main.js
@@ -87,28 +87,26 @@ let olympicsAnswers = {
 
 /* Modal Functionality */
 
-// Get the modal
-const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-const btn = document.querySelector("modal-btn");
+const modalScreen = document.getElementById('#myModal');
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+// Creates an array of elements that have the class .modal-btn
+const btns = document.querySelectorAll('.modal-btn');
 
-// When the user clicks the button, open the modal 
-btn.addEventListener('click', () => {
-  modal.style.display = "block";
+const exitBtn = document.querySelector('.exit');
+
+// For each statement: it loops through the btns array of elements 
+// and adds an event listener to each one. 
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        console.log(e.target)
+    })
 })
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
+/*
+exitBtn.addEventListener('click', () => {
+    modalScreen.style.display = "none";
+})*/
+
