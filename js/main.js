6c2e20913cd0305f02bcdd267ee5f67f727c6219
leer <ladesigns90@gmail.com>
2020-02-09 23:43:40 -0500

    created a map function to create a new array that has a copy of just the answers. Created foreach loop with if conditional to check if answer input is wrong or right. Did not get it orking yet.

--- js/main.js
+++ js/main.js
@@ -2,443 +2,233 @@
 let categoryData = [
     {
         id: "routeFifa10",
-        questionAnswers:
-        {
-            questions: "Where was the first-ever World Cup held?",
-            rightAnswer: [
-                {
-                    answer: "Brazil",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 10
+        questions: "Where was the first-ever World Cup held?",
+        answer: "Brazil",
+        isCorrect: true,
+        isWrong: false,
+        points: 10
             
-            
-        }
     },
     {
         id: "routeFifa30",
-        questionAnswers:
-        {
-            questions: "Who is the Champion of FIFA World Cup 2006?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 30
-            
-            
-        }
+        questions: "Who is the Champion of FIFA World Cup 2006?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 30
+        
     },
     {
         id: "routeFifa50",
-        questionAnswers:
-        {
-            questions: "Who is the Champion of FIFA World Cup 2010 South Africa?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 50
-            
-            
-        }
+        questions: "Who is the Champion of FIFA World Cup 2010 South Africa?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 50
+        
     },
     {
         id: "routeFifa100",
-        questionAnswers:
-        {
-            questions: "What was the score between Brazil and Italy in the 1970 Final?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 100
+        questions: "What was the score between Brazil and Italy in the 1970 Final?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 100
             
-            
-        }
     },
     {
         id: "routeCfb10",
-        questionAnswers:
-        {
-            questions: "Which of these teams is not in the SEC?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 10
-            
-            
-        }
+        questions: "Which of these teams is not in the SEC?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 10
+        
     },
     {
         id: "routeCfb30",
-        questionAnswers:
-        {
-            questions: "Which bowl game is played in Tampa, Florida (as of 2001)?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 30
+        questions: "Which bowl game is played in Tampa, Florida (as of 2001)?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 30
             
-            
-        }
     },
     {
         id: "routeCfb50",
-        questionAnswers:
-        {
-            questions: "What two schools played in the first ever college football game in 1869?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 50
+        questions: "What two schools played in the first ever college football game in 1869?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 50
             
-            
-        }
     },
     {
         id: "routeCfb100",
-        questionAnswers:
-        {
-            questions: "Which of these 4 major colleges did NOT put a QB into the NFL in the 1990s?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 100
-            
-            
-        }
+        questions: "Which of these 4 major colleges did NOT put a QB into the NFL in the 1990s?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 100
+        
     },
     {
         id: "routeNfl10",
-        questionAnswers:
-        {
-            questions: "Which of these teams is not in the SEC?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 10
+        questions: "Which of these teams is not in the SEC?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 10
             
-            
-        }
     },
     {
         id: "routeNfl30",
-        questionAnswers:
-        {
-            questions: "This franchise played in Cleveland, Los Angeles, and St. Louis. Which team is this?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 30
-            
-            
-        }
+        questions: "This franchise played in Cleveland, Los Angeles, and St. Louis. Which team is this?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 30
+        
     },
     {
         id: "routeNfl50",
-        questionAnswers:
-        {
-            questions: "Which of the following is NOT an NFL team name?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-
-                }
-
-            ],
-            points: 50
-            
-            
-        }
+        questions: "Which of the following is NOT an NFL team name?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 50
+        
     },
     {
         id: "routeNfl100",
-        questionAnswers:
-        {
-            questions: "The Miami Dolphins began their franchise in 1966 in the AFL. They joined the NFL in 1970 under the helm of which head coach?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 100
-            
-            
-        }
+        questions: "The Miami Dolphins began their franchise in 1966 in the AFL. They joined the NFL in 1970 under the helm of which head coach?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 100
+        
     },
     {
         id: "routeNba10",
-        questionAnswers:
-        {
-            questions: "Everyone knows about Dr. J, but do you know his real name?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 10
-            
-            
-        }
+        questions: "Everyone knows about Dr. J, but do you know his real name?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 10
+        
     },
     {
         id: "routeNba30",
-        questionAnswers:
-        {
-            questions: "What was the original name of the NBA?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 30
-            
-            
-        }
+        questions: "What was the original name of the NBA?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 30
+       
     },
     {
         id: "routeNba50",
-        questionAnswers:
-        {
-            questions: "What is Wilt Chamberlain's record for most points in a game?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 50
-            
-            
-        }
+        questions: "What is Wilt Chamberlain's record for most points in a game?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 50
+        
     },
     {
         id: "routeNba100",
-        questionAnswers:
-        {
-            questions: "What was the original name of the NBA?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 100
-            
-            
-        }
+        questions: "What was the original name of the NBA?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 100
+        
     },
     {
         id: "routeMlb10",
-        questionAnswers:
-        {
-            questions: "In the 2000s decade, which team other than the Yankees won two World Series?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 10
-            
-            
-        }
+        questions: "In the 2000s decade, which team other than the Yankees won two World Series?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 10
+        
     },
     {
         id: "routeMlb30",
-        questionAnswers:
-        {
-            questions: "Which American League team represented their league in the 2014 World Series?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 30
-            
-            
-        }
+        questions: "Which American League team represented their league in the 2014 World Series?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 30
+        
     },
     {
         id: "routeMlb50",
-        questionAnswers:
-        {
-            questions: "Which team never had a winning record in a season during the 2000s decade?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 50
-            
-            
-        }
+        questions: "Which team never had a winning record in a season during the 2000s decade?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 50
+        
     },
     {
         id: "routeMlb100",
-        questionAnswers:
-        {
-            questions: "Who was the first player to hit 50 home runs and 50 doubles in a season?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 100
-            
-            
-        }
+        questions: "Who was the first player to hit 50 home runs and 50 doubles in a season?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 100
+        
     },
     {
         id: "routeOlympics10",
-        questionAnswers:
-        {
-            questions: "Who won the Olympics 100m race in London 2012?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 10
-            
-            
-        }
+        questions: "Who won the Olympics 100m race in London 2012?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 10
+        
     },
     {
         id: "routeOlympics30",
-        questionAnswers:
-        {
-            questions: "Where was the summer olympics hosted in 1996?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 30
-            
-            
-        }
+        questions: "Where was the summer olympics hosted in 1996?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 30
+        
     },
     {
         id: "routeOlympics50",
-        questionAnswers:
-        {
-            questions: "Which athlete has won the most Olympic medals?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 50
-            
-            
-        }
+        questions: "Which athlete has won the most Olympic medals?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 50
+        
     },
     {
         id: "routeOlympics100",
-        questionAnswers:
-        {
-            questions: "Which of these countries has not hosted the summer Olympics?",
-            rightAnswer: [
-                {
-                    answer: "right answer",
-                    isCorrect: true,
-                    isWrong: false
-                }
-
-            ],
-            points: 100
-            
-            
-        }
+        questions: "Which of these countries has not hosted the summer Olympics?",
+        answer: "right answer",
+        isCorrect: true,
+        isWrong: false,
+        points: 100
+        
     }
     
 
 
 ] //End of categoryData array of nested objects
 
+/*
+const getRightAnswers = categoryData.map(answers) => {
+    return questionAnswers.rightAnswer[0];
+}
 
+console.log(getRightAnswers);
+
+*/
 
 
 // Added click event to all elements with the modal-btn class. 
@@ -450,7 +240,7 @@ categoryBox.forEach(box => {
         
         for(i=0; i < categoryData.length; i++) {
             if (e.currentTarget.id === categoryData[i].id) {
-                let newParagraph = categoryData[i].questionAnswers.questions;
+                let newParagraph = categoryData[i].questions;
                 document.querySelector('#myModal .question').textContent = newParagraph;
                 
             }
@@ -459,6 +249,12 @@ categoryBox.forEach(box => {
 
     })
 })
+
+
+//Create a new array with just the right answers
+const rightAnswer = categoryData.map(function(newArr) {
+    return newArr.answer;
+});
 
 
 
@@ -483,27 +279,51 @@ btns.forEach(btn => {
 
 submitBtn.addEventListener('click', () => {
     // Storing all input element values in captureAnswer variable
-    let captureAnswer = document.querySelector('.answers').value;
-    for(i=0; i < categoryData.length; i++) {
-        let validateAnswer = categoryData[i].questionAnswers.rightAnswer;
+    const captureAnswer = document.querySelector('.answers').value;
+    console.log(captureAnswer);
+
+
+    rightAnswer.forEach((validate) => {
+        if(captureAnswer == rightAnswer) {
+            console.log('Your right!');
+        } 
+    })
+    
+    /*for(i=0; i < categoryData.length; i++) {
+        let validateAnswer = categoryData[i].questionAnswers.rightAnswer[0].answer;
         //console.log(captureAnswer);
         console.log(validateAnswer);
 
-        if(captureAnswer === validateAnswer) {
+
+
+
+
+        /*
+        if(captureAnswer !== validateAnswer) {
             
-            console.log(isCorrect);
+            return "Wrong Answer!";
+            
 
         } else {
             
+              return   // console.log(categoryData[i].questionAnswers.rightAnswer[0].isWrong);
+            
             
 
-        }
+        }*/
 
-    }
+    //}
+
+    /*while(categoryData[i].questionAnswers.rightAnswer[0].answer < 25) {
+
+
+
+    }*/
     
 
     modalScreen.style.display = "none";
 })
+
 
 
 // User must have atleast one answer selected to submit
